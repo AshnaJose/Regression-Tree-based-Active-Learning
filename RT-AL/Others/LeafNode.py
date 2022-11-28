a4a631293f67336005df285efc9b0bd70f8d620a
AshnaJose <92301787+AshnaJose@users.noreply.github.com>
2022-11-28 09:01:30 +0100

    Add files via upload

---
+++ RT-AL/Others/LeafNode.py
@@ -0,0 +1,129 @@
+import random
+import math
+import copy
+
+class LeafNode:
+
+    '''Nodes for tree leaves
+
+    Args:
+        labelled_index (list): Index for labelled data points in the leaf.
+        unlabelled_index (list): Index for unlabelled data points in the leaf.
+        linear_dims (list): A p dim list of 2 dim lists indicating the upper and 
+        lower bounds of the leaf.
+        parent_node (SplitNode): This node's parent
+        parent_branch (int): This node's branch (0 for left, 1 for right)
+    '''
+
+    # leaf_ids = 0
+
+    def __init__(self, labelled_index=None, unlabelled_index=None,linear_dims=None,
+        parent_node=None, parent_branch=None):
+        self.labelled_index = copy.deepcopy(labelled_index)
+        if self.labelled_index is None:
+            self.labelled_index = []
+        self.unlabelled_index = copy.deepcopy(unlabelled_index)
+        if self.unlabelled_index is None:
+            self.unlabelled_index = []
+        self.linear_dims = copy.deepcopy(linear_dims)
+        if self.linear_dims is None:
+            self.linear_dims = []
+        self.parent_node = parent_node
+        self.parent_branch = parent_branch
+        self.subtree_linear_dim = self.calculate_subtree_linear_dim()
+
+        # LeafNode.leaf_ids+= 1
+        # self.leaf_id = LeafNode.leaf_ids
+        self.leaf_id = None
+
+        self.full_leaf_list_pos = None
+
+    def __str__(self):
+        print_str = (
+        'n_labelled = {}\n'
+        'n_unlabelled = {}\n'
+        'leaf_id = {}'.format(
+            len(self.labelled_index), 
+            len(self.unlabelled_index),
+            self.leaf_id))
+        return(print_str)
+
+    def rounded_linear_dims(self, sig_fig = 2):
+        print_list = [[round(x[0],sig_fig), round(x[1], sig_fig)] for x in self.linear_dims]
+        return print_list
+    
+    def pick_new_points(self, num_samples, self_update = False, set_seed = None):
+        '''Returns the index of points to get labels for, and automatically adds
+        them the leafs labelled points by default
+        '''
+
+        num_samples = copy.copy(num_samples)
+
+        if num_samples > len(self.unlabelled_index):
+            raise ValueError('This leaf only has {} < {} unlabelled points'.format(
+                len(self.unlabelled_index), num_samples))
+
+        if set_seed is not None:
+            random.seed(set_seed)
+
+        new_points = random.sample(self.unlabelled_index, num_samples)
+        if self_update:
+            self.extend_labelled_index(new_points)
+            self.unlabelled_index = [x for x in self.unlabelled_index if x not in new_points]
+
+        return new_points
+
+    def calculate_subtree_linear_dim(self):
+        '''Since this 'subtree' is only the leaf, it's linear dim is just the
+        sum of its linear dims.
+        '''
+
+        tot = 0
+        for pair in self.linear_dims:
+            tot += abs(pair[1] - pair[0])
+
+        return tot
+
+    def make_labelled(self, index):
+        '''Takes a point in the unlablled_index list and moves it to the labelled one.
+        If the point is not in the unlabelled_index list it returns an error.
+        '''
+
+        index = copy.copy(index)
+
+        if index not in self.unlabelled_index:
+            raise ValueError('Point {} is not in this leaf'.format(index))
+
+        self.labelled_index.append(index)
+        self.unlabelled_index.remove(index)
+
+    ###########################################
+
+    # Basic methods
+
+    def is_leaf(self):
+        return True
+
+    def leaf_for_point(self, data_point):
+        return self
+
+    def extend_labelled_index(self, new_labelled_list):
+        new_labelled_list = copy.deepcopy(new_labelled_list)
+        self.labelled_index.extend(new_labelled_list)
+
+    def extend_unlabelled_index(self, new_unlabelled_list):
+        new_unlabelled_list = copy.deepcopy(new_unlabelled_list)
+        self.unlabelled_index.extend(new_unlabelled_list)
+
+    def set_linear_dims(self, new_linear_dims):
+        new_linear_dims = copy.deepcopy(new_linear_dims)
+        self.linear_dims = new_linear_dims
+        self.subtree_linear_dim = self.calculate_subtree_linear_dim()
+
+    def calculate_cell_l2_diameter(self):
+
+        tot = 0
+        for pair in self.linear_dims:
+            tot += (pair[1] - pair[0])**2
+
+        return math.sqrt(tot)
---
+++ RT-AL/Others/SplitNode.py
@@ -0,0 +1,64 @@
+import copy
+
+class SplitNode:
+
+    '''Nodes for the tree interior
+
+    Args:
+        split_dim (int): The dimension this node split on (starts at 0)
+        split_val (float): The value of the split point
+        parent_node (SplitNode): This node's parent node
+        parent_branch (int): This node's branch (0 for left, 1 for right)
+    '''
+
+    def __init__(self, split_dim, split_val,left_child, right_child, 
+        parent_node=None, parent_branch=None, subtree_linear_dim=None):
+        self.split_dim = copy.copy(split_dim)
+        self.split_val = copy.copy(split_val)
+        self.parent_node = parent_node
+        self.parent_branch = parent_branch
+        self.left_child = left_child
+        self.right_child = right_child
+        self.subtree_linear_dim = subtree_linear_dim
+
+    def __str__(self):
+        return 'Splits on dimension {} and value {}'.format(self.split_dim, self.split_val)
+
+    def calculate_subtree_linear_dim(self):
+        '''Note this is likely to be very expensive. Ideally the subtree linear dim value
+        should be updated as you build the tree percolating up
+        '''
+
+        self.subtree_linear_dim = (self.left_child.calculate_subtree_linear_dim() +
+                                   self.right_child.calculate_subtree_linear_dim())
+        return self.subtree_linear_dim
+
+    def percolate_subtree_linear_dim_change(self, val):
+        '''Changes the value of the subtree linear dimension by val for this tree
+        and all it's parents. Should be the more efficient way of keeping track of
+        subtree_linear_dim during tree building time.
+        '''
+        val = copy.copy(val)
+        self.subtree_linear_dim += val
+        if self.parent_node is not None:
+            self.parent_node.percolate_subtree_linear_dim_change(val)
+
+    def leaf_for_point(self, data_point):
+        if data_point[self.split_dim] < self.split_val:
+            if self.left_child.is_leaf():
+                return self.left_child
+            else:
+                return self.left_child.leaf_for_point(data_point)
+
+        else:
+            if self.right_child.is_leaf():
+                return self.right_child
+            else:
+                return self.right_child.leaf_for_point(data_point)
+
+    ###########################################
+
+    # Basic methods
+
+    def is_leaf(self):
+        return False
